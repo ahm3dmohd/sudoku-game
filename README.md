f068ca6b22e43e1d57c164e7389cb732bf7623ca
Ahmed <ahmedmohdqamber@gmail.com>
2026-08-10 20:01:32 +0300

    finished the renderBoard function and selectCell function which deselects the current cell and picks the cell you selected using remove and add attributes and an event listener for the number pad

---
+++ README.md
@@ -0,0 +1,26 @@
+# Sudoku 
+
+## About The Game 
+
+Sudoku is a single-player puzzle game played on a 9x9 grid divided into a 3x3 grid where you have to fill every row and column with numbers 1-9 without any repeating in any box, column or row.
+
+This is a browser based game of sudoku which is based on HTML , CSS , JavaScript. You pick the cell and fill it up with a number and it will validate it using JavaScript and the game's rules. I chose sudoku because it has clear winning conditions and clear logic which makes it a bit easier to program. 
+
+## Getting Started 
+
+https://github.com/ahm3dmohd/sudoku-game.git
+
+## Attributions 
+
+
+## Technologies Used 
+
+- HTML 
+- CSS 
+- JavaScript 
+- Visual Code Studio (IDE)
+
+## Next steps
+
+
+
--- app.js
+++ app.js
@@ -23,13 +23,68 @@ const solution = [
 ]
 
 const board = document.getElementById("board");
+const numberPad = document.getElementById("number-pad");
+
+let selectedCell = null;
 
 function renderBoard() {
     board.innerHTML = ''
 
-    for (let row; row < 9; row++) {
-        for (let col; col < 9; col++) {
-            
+    for (let row = 0; row < 9; row++) {
+        for (let col = 0; col < 9; col++) {
+            const cell = document.createElement("div")
+
+            cell.classList.add("cell")
+
+            cell.dataset.row = row
+            cell.dataset.col = col
+
+            const value = puzzle[col][row]
+
+            if (value !== 0) {
+                cell.textContent = value
+                cell.classList.add("given")
+            }
+
+            if ((col + 1) % 3 === 0 && col !== 8) {
+            cell.classList.add("border-right-thick");
+            }
+
+            if ((row + 1) % 3 === 0 && row !== 8) {
+            cell.classList.add("border-bottom-thick");
+            }
+
+            cell.addEventListener("click", () => selectCell(cell));
+
+            board.appendChild(cell);
+
+            }
         }
     }
+
+function selectCell(cell) {
+    if (selectedCell) {
+        selectedCell.classList.remove("selected")
+    }
+
+    selectedCell = cell
+    selectedCell.classList.add("selected")
 }
+
+numberPad.addEventListener("click", (event) => {
+    const button = event.target.closest(".num-btn")
+
+    if (!button) return
+    if (!selectedCell) return
+    if (selectedCell.classList.contains("given")) return
+
+    const number = button.dataset.number
+
+    if (number === "0") {
+        selectedCell.textContent = ""
+    } else {
+        selectedCell.textContent = number
+    }
+})
+
+renderBoard()
