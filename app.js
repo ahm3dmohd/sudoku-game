c2059e44585a7835533b5f2177ced2689f6830df
Ahmed <ahmedmohdqamber@gmail.com>
2026-08-11 20:17:02 +0300

    did the checkPuzzle function and linked it to the button with conditions to check if the cell is empty or filled and when its filled it checks if its correct or invalid and assigns it to the cell with the css properties .invalid and .correct

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// 0 means empty
+
 const puzzle = [
   [5, 3, 0, 0, 7, 0, 0, 0, 0],
   [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -7,8 +9,8 @@ const puzzle = [
   [7, 0, 0, 0, 2, 0, 0, 0, 6],
   [0, 6, 0, 0, 0, 0, 2, 8, 0],
   [0, 0, 0, 4, 1, 9, 0, 0, 5],
-  [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
+  [0, 0, 0, 0, 8, 0, 0, 7, 9],
+];
 
 const solution = [
   [5, 3, 4, 6, 7, 8, 9, 1, 2],
@@ -19,72 +21,106 @@ const solution = [
   [7, 1, 3, 9, 2, 4, 8, 5, 6],
   [9, 6, 1, 5, 3, 7, 2, 8, 4],
   [2, 8, 7, 4, 1, 9, 6, 3, 5],
-  [3, 4, 5, 2, 8, 6, 1, 7, 9]
-]
+  [3, 4, 5, 2, 8, 6, 1, 7, 9],
+];
+const checkBtn = document.getElementById("check-btn");
 
 const board = document.getElementById("board");
+
 const numberPad = document.getElementById("number-pad");
 
 let selectedCell = null;
 
-function renderBoard() {
-    board.innerHTML = ''
-
-    for (let row = 0; row < 9; row++) {
-        for (let col = 0; col < 9; col++) {
-            const cell = document.createElement("div")
-
-            cell.classList.add("cell")
-
-            cell.dataset.row = row
-            cell.dataset.col = col
-
-            const value = puzzle[col][row]
-
-            if (value !== 0) {
-                cell.textContent = value
-                cell.classList.add("given")
-            }
-
-            if ((col + 1) % 3 === 0 && col !== 8) {
-            cell.classList.add("border-right-thick");
-            }
-
-            if ((row + 1) % 3 === 0 && row !== 8) {
-            cell.classList.add("border-bottom-thick");
-            }
-
-            cell.addEventListener("click", () => selectCell(cell));
-
-            board.appendChild(cell);
-
-            }
-        }
-    }
-
-function selectCell(cell) {
-    if (selectedCell) {
-        selectedCell.classList.remove("selected")
-    }
-
-    selectedCell = cell
-    selectedCell.classList.add("selected")
-}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 numberPad.addEventListener("click", (event) => {
-    const button = event.target.closest(".num-btn")
+  const button = event.target.closest(".num-btn");
 
-    if (!button) return
-    if (!selectedCell) return
-    if (selectedCell.classList.contains("given")) return
+  if (!button) return;
+  if (!selectedCell) return;
+  if (selectedCell.classList.contains("given")) return;
 
-    const number = button.dataset.number
+  const number = button.dataset.number;
 
-    if (number === "0") {
-        selectedCell.textContent = ""
-    } else {
-        selectedCell.textContent = number
-    }
+  if (number === "0") {
+    selectedCell.textContent = "";
+  } else {
+    selectedCell.textContent = number;
+  }
+});
+
+checkBtn.addEventListener("click", () => {
+    checkPuzzle()
 })
 
-renderBoard()
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function renderBoard() {
+  board.innerHTML = "";
+
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      const cell = document.createElement("div");
+
+      cell.classList.add("cell");
+
+      cell.dataset.row = row;
+      cell.dataset.col = col;
+
+      const value = puzzle[col][row];
+
+      if (value !== 0) {
+        cell.textContent = value;
+        cell.classList.add("given");
+      }
+
+      if ((col + 1) % 3 === 0 && col !== 8) {
+        cell.classList.add("border-right-thick");
+      }
+
+      if ((row + 1) % 3 === 0 && row !== 8) {
+        cell.classList.add("border-bottom-thick");
+      }
+
+      cell.addEventListener("click", () => selectCell(cell));
+
+      board.appendChild(cell);
+    }
+  }
+}
+
+function selectCell(cell) {
+  if (selectedCell) {
+    selectedCell.classList.remove("selected");
+  }
+
+  selectedCell = cell;
+  selectedCell.classList.add("selected");
+}
+
+renderBoard();
+
+function checkPuzzle() {
+  const cells = board.querySelectorAll(".cell");
+
+  for (let singleCell of cells) {
+    const row = Number(singleCell.dataset.row);
+    const col = Number(singleCell.dataset.col);
+
+    const answer = solution[col][row];
+
+    if (singleCell.textContent === "") {
+      continue; // skip empty cells
+    }
+    // this checks if the cell answer is same to the solution
+    if (Number(singleCell.textContent) === answer) {
+      singleCell.classList.remove("invalid");
+      singleCell.classList.add("correct")
+    }
+    // this else is if its not the same it directly has it as invalid..
+    else {
+        singleCell.classList.remove("correct")
+      singleCell.classList.add("invalid");
+    }
+  }
+}
